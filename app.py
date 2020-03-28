28cdeeb7b0cd64aa4e4c6a324e3f2714a761b75e
Paul Vonder Haar <pvonhaar@gmail.com>
2020-03-27 19:11:21 -0500

    Attempting a different method, testing this now.

--- app.py
+++ app.py
@@ -1,43 +1,35 @@
 import os
-import inspect
-import boto3
-import psycopg2
-from flask import Flask, jsonify, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, send_file
+from s3_demo import list_files, download_file, upload_file
 
 app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+BUCKET = "flaskdrive"
 
+@app.route('/')
+def entry_point():
+    return 'Hello World!'
 
-#################################################
-# Database Setup
-#################################################
+@app.route("/storage")
+def storage():
+    contents = list_files("flaskdrive")
+    return render_template('storage.html', contents=contents)
 
+@app.route("/upload", methods=['POST'])
+def upload():
+    if request.method == "POST":
+        f = request.files['file']
+        f.save(os.path.join(UPLOAD_FOLDER, f.filename))
+        upload_file(f"uploads/{f.filename}", BUCKET)
 
-@app.route("/")
-def index():
-    """Return the homepage."""
-    return render_template("index.html")
+        return redirect("/storage")
 
+@app.route("/download/<filename>", methods=['GET'])
+def download(filename):
+    if request.method == 'GET':
+        output = download_file(filename, BUCKET)
 
-@app.route("/submit/",methods=['POST'])
-def chart():
-    file=request.files("input_image")
-    access_key='AKIAQFSEYOHOTL5OCAYD'
-    secret_key='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'
-    s3 = boto3.client('s3',aws_access_key_id=access_key, aws_secret_access_key=secret_key)  
-    try:
-        s3.upload_file(file,'memorialphotos','test.jpg')
-        return "True"
-    except FileNotFoundError:
-        print("The file was not found")
-        return "False"
-    except NoCredentialsError:
-        print("Credentials not available")
-        return "False"
+        return send_file(output, as_attachment=True)
 
-
-
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
